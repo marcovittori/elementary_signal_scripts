62a11d913f241223bd8c7e8a184cf29558f69f69
marcovittori <131837019+marcovittori@users.noreply.github.com>
2023-04-27 14:00:00 -0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,147 +6,66 @@ Created on Tue Apr 25 23:52:16 2023
 """
 import numpy as np
 
-def unit_impulse(length, amplitude=1, shift=0):
+def unit_impulse(start=-10, end=10, amplitude=1):
     """
-    Generate a unit impulse signal.
+    Creates a unit impulse signal.
 
-    Parameters
-    ----------
-    length : int
-        Length of the signal.
-    amplitude : float, optional
-        Amplitude of the impulse (default is 1).
-    shift : int, optional
-        Amount to shift the impulse (default is 0).
+    Parameters:
+    start (int): the starting value of the signal (default=-10)
+    end (int): the ending value of the signal (default=10)
+    amplitude (float): the amplitude of the impulse (default=1)
 
-    Returns
-    -------
-    signal : numpy.ndarray
-        A 1D numpy array of length `length` containing the generated
-        unit impulse signal.
+    Returns:
+    tuple: A tuple containing two numpy arrays. The first array contains the
+    sample values and the second array contains the signal values.
     """
-    signal = np.zeros(length)
-    signal[shift] = amplitude
-    return signal
+    samples = np.arange(start,end+1,1)
+    signal = np.zeros(len(samples))
+    mask = samples == 0
+    signal[mask] = amplitude
+    
+    return samples, signal
 
 
-def heavy_side(length, amplitude=1, shift=0):
+def heavy_side(start=-10, end=10, amplitude=1):
     """
-    Generate a unit step signal.
+    Creates a Heaviside step function.
 
-    Parameters
-    ----------
-    length : int
-        Length of the signal.
-    amplitude : float, optional
-        Amplitude of the step (default is 1).
-    shift : int, optional
-        Amount to shift the step (default is 0).
+    Parameters:
+    start (int): the starting value of the signal (default=-10)
+    end (int): the ending value of the signal (default=10)
+    amplitude (float): the amplitude of the Heaviside function (default=1)
 
-    Returns
-    -------
-    signal : numpy.ndarray
-        A 1D numpy array of length `length` containing the generated
-        unit step signal.
+    Returns:
+    tuple: A tuple containing two numpy arrays. The first array contains the
+    sample values and the second array contains the signal values.
     """
-    signal = np.zeros(length)
-    signal[shift:] = amplitude
-    return signal
+    samples = np.arange(start,end+1,1)
+    signal = np.zeros(len(samples))
+    mask = samples > 0
+    signal[mask] = amplitude
+    
+    return samples, signal
 
 
-def rectangular_pulse(length, amplitude=1, shift=0, width=1):
+def rectangular_pulse(width, start=-5, end=5, amplitude=1):
     """
-    Generate a rectangular pulse signal.
+    Creates a rectangular pulse signal.
 
-    Parameters
-    ----------
-    length : int
-        Length of the signal.
-    amplitude : float, optional
-        Amplitude of the pulse (default is 1).
-    shift : int, optional
-        Amount to shift the pulse (default is 0).
-    width : int, optional
-        Width of the pulse (default is 1).
+    Parameters:
+    width (int): the width of the pulse
+    start (int): the starting value of the signal (default=-5)
+    end (int): the ending value of the signal (default=5)
+    amplitude (float): the amplitude of the pulse (default=1)
 
-    Returns
-    -------
-    signal : numpy.ndarray
-        A 1D numpy array of length `length` containing the generated
-        rectangular pulse signal.
+    Returns:
+    tuple: A tuple containing two numpy arrays. The first array contains the
+    sample values and the second array contains the signal values.
     """
-    signal = np.zeros(length)
-    signal[shift:shift+width] = amplitude
-    return signal
+    samples = np.arange(start, end+1, 1)
+    signal = np.zeros(len(samples))
+    mask = (samples > 0-width-1) & (samples < width+1)
+    signal[mask] = amplitude
+    
+    return samples, signal
 
-
-def triangular_pulse(length, amplitude=1, shift=0, width=1):
-    """
-    Generate a triangular pulse signal.
-
-    Parameters
-    ----------
-    length : int
-        Length of the signal.
-    amplitude : float, optional
-        Amplitude of the pulse (default is 1).
-    shift : int, optional
-        Amount to shift the pulse (default is 0).
-    width : int, optional
-        Width of the pulse (default is 1).
-
-    Returns
-    -------
-    signal : numpy.ndarray
-        A 1D numpy array of length `length` containing the generated
-        triangular pulse signal.
-    """
-    signal = np.zeros(length)
-    t = np.linspace(-width/2, width/2, width)
-    signal[shift:shift+width] = amplitude * (1 - np.abs(t) / (width/2))
-    return signal
-
-def exponential_signal(length, amplitude = 1, decay_rate = 1):
-    """
-    Generate an exponential signal.
-
-    Parameters
-    ----------
-    length : int
-        Length of the signal.
-    amplitude : float, optional
-        Amplitude of the signal (default is 1).
-    decay_rate : float, optional
-        Decay rate of the exponential function (default is 1).
-
-    Returns
-    -------
-    signal : numpy.ndarray
-        A 1D numpy array of length `length` containing the generated
-        exponential signal.
-    """
-    n = np.arange(length)
-    signal = amplitude * np.exp(-decay_rate * n)
-    return signal
-
-def normal_signal(length, mean, std_dev):
-    """
-    Generate a random normal signal.
-
-    Parameters
-    ----------
-    length : int
-        Length of the signal.
-    mean : float
-        Mean of the normal distribution.
-    std_dev : float
-        Standard deviation of the normal distribution.
-
-    Returns
-    -------
-    signal : numpy.ndarray
-        A 1D numpy array of length `length` containing the generated
-        random normal signal.
-    """
-    signal = np.random.normal(mean, std_dev, size=length)
-    return signal
